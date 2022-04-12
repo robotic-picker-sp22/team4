22b4e58a9a87e422b60e1e0d0fa959fb059ef119
Tanish Kapur <tanishkapur08@gmail.com>
2022-04-12 16:14:43 -0700

    Finished Labs 11/12

---
+++ applications/scripts/marker_demo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import rospy
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Quaternion, Pose, Point, Vector3
+from std_msgs.msg import Header, ColorRGBA
+
+def wait_for_time():                                              
+    """Wait for simulated time to begin.                          
+    """                                                           
+    while rospy.Time().now().to_sec() == 0:                       
+        pass
+
+def show_text_in_rviz(marker_publisher, text):
+    marker = Marker(
+                type=Marker.TEXT_VIEW_FACING,
+                id=0,
+                lifetime=rospy.Duration(3),
+                pose=Pose(Point(0.75, 0.5, 1.45), Quaternion(0, 0, 0, 1)),
+                scale=Vector3(0.2, 0.2, 0.2),
+                header=Header(frame_id='base_link'),
+                color=ColorRGBA(1.0, 1.0, 0.0, 0.8),
+                text=text)
+    marker_publisher.publish(marker)
+
+def main():
+  rospy.init_node('my_node')
+  marker_publisher = rospy.Publisher('visualization_marker', Marker)
+  rospy.sleep(0.5)
+  show_text_in_rviz(marker_publisher, 'Hello world!')
+  wait_for_time()
+
+if __name__ == '__main__':
+  main()
---
+++ applications/scripts/path_tracker.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import rospy
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Quaternion, Pose, Point, Vector3
+from std_msgs.msg import Header, ColorRGBA
+from nav_msgs.msg import Odometry
+
+
+class NavPath(object):
+    def __init__(self):
+        self._path = []
+        self._publisher = rospy.Publisher('visualization_marker', Marker)
+    
+    def callback(self, msg):
+        rospy.loginfo(msg)
+        if len(self._path) == 0 or self.dist(self._path[-1], msg.pose.pose.position):
+            self._path.append(msg.pose.pose.position)
+        if len(self._path) > 1:
+            self.publish()
+
+    def dist(self, P1, P2):
+        d = (P1.x - P2.x) ** 2 + (P1.y - P2.y) ** 2 + (P1.z - P2.z) ** 2
+        return d > 0.5
+
+    def publish(self):
+        marker = Marker(
+                    type=Marker.LINE_STRIP,
+                    id=27,
+                    pose=Pose(Point(0, 0, 0), Quaternion(0, 0, 0, 0)),
+                    points=self._path,
+                    scale=Vector3(0.1, 0, 0),
+                    header=Header(frame_id='odom'),
+                    color=ColorRGBA(0.0, 1.0, 0.0, 0.8),
+                    text="")
+        self._publisher.publish(marker)
+
+
+def main():
+    rospy.init_node('my_node')
+    nav_path = NavPath()
+    rospy.sleep(0.5)
+    rospy.Subscriber('odom', Odometry, nav_path.callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+  main()
